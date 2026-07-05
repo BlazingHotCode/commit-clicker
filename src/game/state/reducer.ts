7a85f0c19cac00d6691c1835b706916ec5bfe0cd
BlazingHotCode <nadav.babad@gmail.com>
2026-07-05 17:24:12 +0300

    added BUY_UPGRADE to reducer

--- src/game/state/reducer.ts
+++ src/game/state/reducer.ts
@@ -1,3 +1,5 @@
+import { upgrades } from "../data/upgrades";
+import { getUpgradeCost } from "../engine/formulas";
 import type { GameAction } from "./actions";
 import { initialState } from "./initialState";
 import type { GameState } from "./types";
@@ -25,6 +27,60 @@ export function gameReducer(state: GameState, action: GameAction): GameState {
         reputation: state.reputation + state.reputationPerBug,
         totalBugsFixed: state.totalBugsFixed + 1,
       };
+    }
+
+    case "BUY_UPGRADE": {
+      const upgrade = upgrades.find((item) => item.id === action.upgradeId);
+      if (!upgrade) return state;
+
+      const currentLevel = state.upgrades[action.upgradeId];
+      const cost = getUpgradeCost(upgrade, currentLevel);
+
+      if (state.linesOfCode < cost) return state;
+
+      const nextState: GameState = {
+        ...state,
+        linesOfCode: state.linesOfCode - cost,
+        upgrades: {
+          ...state.upgrades,
+          [action.upgradeId]: currentLevel + 1,
+        },
+      };
+
+      switch (action.upgradeId) {
+        case "mechanicalKeyboard":
+          return {
+            ...nextState,
+            locPerClick: nextState.locPerClick + 1,
+          };
+
+        case "autocomplete":
+          return {
+            ...nextState,
+            locPerClick: nextState.locPerClick + 5,
+          };
+
+        case "juniorDev":
+          return {
+            ...nextState,
+            locPerClick: nextState.locPerClick + 1,
+          };
+
+        case "unitTests":
+          return {
+            ...nextState,
+            bugChance: Math.max(0.01, nextState.bugChance - 0.01),
+          };
+
+        case "debugger":
+          return {
+            ...nextState,
+            reputationPerBug: nextState.reputationPerBug + 1,
+          };
+
+        default:
+          return nextState;
+      }
     }
 
     case "TICK": {
