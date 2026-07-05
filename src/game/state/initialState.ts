e6534d271986bab13c302108848fc3c57933a93b
BlazingHotCode <nadav.babad@gmail.com>
2026-07-05 16:54:35 +0300

    added initial state

---
+++ src/game/state/initialState.ts
@@ -0,0 +1,23 @@
+import type { GameState } from "./types";
+
+export const initialState: GameState = {
+  linesOfCode: 0,
+  bugs: 0,
+  reputation: 0,
+
+  totalLinesofCode: 0,
+  totalBugsFixed: 0,
+
+  locPerClick: 1,
+  locPerSecond: 0,
+  bugChance: 0.1,
+  reputationPerBug: 1,
+
+  upgrades: {
+    mechanicalKeyboard: 0,
+    autocomplete: 0,
+    juniorDev: 0,
+    unitTests: 0,
+    debugger: 0,
+  },
+};
