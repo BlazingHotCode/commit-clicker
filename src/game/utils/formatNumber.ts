6ee14674b0edd2b08b858100c915d5487cd8590b
BlazingHotCode <nadav.babad@gmail.com>
2026-07-06 00:08:07 +0300

    show decimal stat values

--- src/game/utils/formatNumber.ts
+++ src/game/utils/formatNumber.ts
@@ -1,13 +1,15 @@
 export function formatNumber(value: number): string {
-    if (value < 1_000) return Math.floor(value).toString();
+  if (value < 1_000) {
+    return Number.isInteger(value) ? value.toString() : value.toFixed(2);
+  }
 
-    if (value < 1_000_000) {
-        return `${(value / 1_000).toFixed(1)}K`
-    }
+  if (value < 1_000_000) {
+    return `${(value / 1_000).toFixed(1)}K`;
+  }
 
-    if (value < 1_000_000_000) {
-        return `${(value / 1_000_000).toFixed(1)}M`
-    }
+  if (value < 1_000_000_000) {
+    return `${(value / 1_000_000).toFixed(1)}M`;
+  }
 
-    return `${(value / 1_000_000_000).toFixed(1)}B`
+  return `${(value / 1_000_000_000).toFixed(1)}B`;
 }
